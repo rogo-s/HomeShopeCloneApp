ecddc9435cda6845e6f7abbb8818d4b81425f9dd
Rogo <rogosubandono513@gmail.com>
2023-04-01 05:35:02 +0700

    Initial commit

---
+++ app/src/main/java/com/ergea/homeshopecloneapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ergea.homeshopecloneapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        this.supportActionBar?.hide()
+        setContentView(R.layout.activity_main)
+    }
+}
